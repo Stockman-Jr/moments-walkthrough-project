26ed2d0aabec2753a925c69dd548629ff79ddc46
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-24 13:50:15 +0000

    set up for api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults';
 
 function App() {
   return (
