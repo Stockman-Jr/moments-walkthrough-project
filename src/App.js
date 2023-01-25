6eaf93406e41aed9d540f791015688e4756c0090
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-25 09:37:45 +0000

    add user context

--- src/App.js
+++ src/App.js
@@ -3,35 +3,17 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
-import './api/axiosDefaults';
+import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
-import { createContext, useEffect, useState } from 'react';
-import axios from 'axios';
 
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
+
 
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  const handleMount = async () => {
-    try{
-      const {data} = await axios.get('dj-rest-auth/user/');
-      setCurrentUser(data);
-    }catch(err){
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
 
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-    <SetCurrentUserContext.Provider value={setCurrentUser}>
+
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
@@ -44,8 +26,6 @@ function App() {
 
       </Container>
     </div>
-    </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
   );
 }
 
