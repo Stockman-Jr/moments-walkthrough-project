52c94806bf78070c266d4bbd8ecc6a9c575af3f9
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-25 13:58:10 +0000

    push

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from '../../components/Avatar';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from "../../styles/Post.module.css";
 
 const Post = (props) => {
-  return (
-    <div>
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    likes_count,
+    like_id,
+    title,
+    content,
+    image,
+    updated_at,
+  } = props;
 
-    </div>
-  )
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  return <Card className={styles.Post}>
+    <Card.Body>
+      <Media className="align-items-center justify-content-between">
+        <Link to={`/posts/${profile_id}`}>
+          <Avatar src={profile_image} height={55}/>
+          {owner}
+        </Link>
+        <div className='d-flex align-items-center'>
+          <span>{updated_at}</span>
+
+        </div>
+      </Media>
+    </Card.Body>
+  </Card>
 };
 
 export default Post;
